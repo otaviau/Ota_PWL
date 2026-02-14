89efd6bf187190ade3abe41c49b0b4652e771590
HP 440 G6 <otavia.ou@gmail.com>
2026-02-14 14:07:57 +0700

    pertemuan01

--- pertemuan01/resources/views/welcome.blade.php
+++ pertemuan01/resources/views/welcome.blade.php
@@ -3,12 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
-
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
         <!-- Styles -->
         <style>
             /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}
